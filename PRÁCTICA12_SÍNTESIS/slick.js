993121059a0e4b71b2e41b8531f258a351d6ff72
Jhen D'Òliver Morious <jandol1996@hotmail.com>
2017-03-29 17:42:13 +0000

    Prática 12 frontend hecho excepto animaciones left/right

--- PRÁCTICA12_SÍNTESIS/slick.js
+++ PRÁCTICA12_SÍNTESIS/slick.js
@@ -1,37 +1,19 @@
 /*global $*/
 $(document).ready(function(){
-    $('#slider2').slick({
-        autoplay: true,
-        dots: true
-    });
-    
-    $('#slider3').slick({
-        autoplay: true,
-        dots: true,
+    $('#slider-imgs').slick({
+        autoplay: false,
+        dots: false,
         slidesToShow: 2,
-        slidesToScroll: 2
-    })
-    
-    $('#slider4').slick({
-        autoplay: true,
-        dots: true,
-        slidesToShow: 4,
         slidesToScroll: 1,
+        adaptiveHeight: true,
         responsive: [
-        {
-          breakpoint: 850,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
         {
           breakpoint: 700,
           settings: {
+            dots: true,
             slidesToShow: 1,
             slidesToScroll: 1
           }
-        }
-        ]
-    })
+        }]
+    });
 })

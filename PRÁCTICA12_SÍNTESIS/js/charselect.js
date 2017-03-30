618cbba303f726fb368b4069ae6baa89c70cbc4f
Jhen D'Òliver Morious <jandol1996@hotmail.com>
2017-03-30 16:28:23 +0000

    Práctica perfecta, pero no entiendo por qué pohohohoho

--- PRÁCTICA12_SÍNTESIS/js/charselect.js
+++ PRÁCTICA12_SÍNTESIS/js/charselect.js
@@ -3,21 +3,8 @@ $(document).ready(init);
 
 function init()
 {
-    $("#player1").click(altColor);
-    $("#player2").click(altColor);
     $("#char1").change(changeImg);
     $("#char2").change(changeImg);
-}
-
-function altColor()
-{
-    switch($(this).css("background-color"))
-    {
-        case "rgb(128, 128, 128)": $(this).css({"background-color":"blue"}); break;
-        case "rgb(0, 0, 255)": $(this).css({"background-color":"green"}); break;
-        case "rgb(0, 128, 0)": $(this).css({"background-color":"red"}); break;
-        case "rgb(255, 0, 0)": $(this).css({"background-color":"blue"}); break;
-    }
 }
 
 function changeImg()
@@ -27,13 +14,15 @@ function changeImg()
         var image = $("<img id='selectedcharimg1' class='selectedchar' src='img/"+$(this).val()+".png'></img>").hide();
         $("#selectedchar1 > img").remove();
         $("#selectedchar1").append(image);
-        image.show(500);
+        image.css({"right":"300%"});
+        image.animate({"right":"0%"},{"duration":500});
     }
     else
     {
         var image = $("<img id='selectedcharimg2' class='selectedchar' src='img/"+$(this).val()+".png'></img>").hide();
         $("#selectedchar2 > img").remove();
         $("#selectedchar2").append(image);
-        image.show(500);
+        image.css({"left":"300%"});
+        image.animate({"left":"0%"},{"duration":500});
     }
 }
